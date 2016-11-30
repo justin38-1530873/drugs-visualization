f58bfac498ae0f442613360d771cccf8b935bfc5
Ian <ifigon@outlook.com>
2016-11-30 00:12:25 -0800

    Added bare server.R and ui.R

---
+++ server.R
@@ -0,0 +1,13 @@
+library(dplyr)
+
+data <- read.csv('./data/2011-2015_Drug_data.csv', stringsAsFactors = FALSE)
+
+
+shinyServer(function(input, output) { 
+  
+  
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(plotly)
+
+shinyUI(navbarPage('Drug Distribution from 2011 to 2015',
+                   tabPanel('Test',
+                            titlePanel('In progress'),
+                            
+                            sidebarLayout(
+                              
+                              sidebarPanel(
+                                
+                              ),
+                              
+                              mainPanel(
+                                
+                              )
+                            )
+                   ), 
+                   
+                   tabPanel('Test2',
+                            
+                            titlePanel('In progress'),
+                            
+                            sidebarLayout(
+                              
+                              sidebarPanel(
+                                
+                              ),
+                              
+                              mainPanel(
+                                
+                              )
+                            )
+                   )
+))
