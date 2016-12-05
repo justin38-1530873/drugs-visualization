ffdb262f6e1eb86bdd2407255f8cf8d5b2231f13
lukasg33 <lukasg33@uw.edu>
2016-12-04 23:35:17 -0800

    working map but need to add other features

--- server.R
+++ server.R
@@ -15,29 +15,19 @@ summary.data <- read.csv('summary.data.csv', stringsAsFactors = FALSE)
 shinyServer(function(input, output) { 
 
   
-  
   output$map <- renderPlot({
    
-   selected.data <-  reactive({
-      if(input$Drug == "Cocaine"){
-        selected.data <- filter(summary.data, Drug.Name == "Cocaine")
-        return(selected.data)
-      }
-      if(input$Drug == "Heroin"){
-        selected.data <- filter(summary.data, Drug.Name == "Heroin")
-        return(selected.data)
-      }
-      else if(input$Drug == "Cannabis Herb (Marijuana)"){
-        selected.data <- filter(summary.data, Drug.Name == "Cannabis Herb (Marijuana)")
-        return(selected.data)
-      }})
+  
+   selected.data <- switch (input$Drug,
+      "Cocaine" = filter(summary.data, Drug.Name == "Cocaine"),
+      "Heroin"= filter(summary.data, Drug.Name == "Heroin"),
+      "Cannabis Herb (Marijuana)" = filter(summary.data, Drug.Name == "Cannabis Herb (Marijuana)")
+   )
     
-   
-    
-    map("world", regions= ".", mar = c(1,1,1,1)*5) 
-    points(x = selected.data$long.country.obtained, y = selected.data$lat.country.obtained, col = "red")
-    points(x =  selected.data$long.destination.country, y = selected.data$lat.destination.country, col = "red")
-    arrows(x0 = selected.data$long.country.obtained, y0 = selected.data$lat.country.obtained, x1 = selected.data$long.destination.country, y1 = selected.data$lat.destination.country, col = "red", lwd = .5)
+    map("world", regions= ".", mar = c(.5,.5,.5,.5))
+   points(x = selected.data$long.country.obtained, y = selected.data$lat.country.obtained, col = "red")
+   points(x =  selected.data$long.destination.country, y = selected.data$lat.destination.country, col = "red")
+   arrows(x0 = selected.data$long.country.obtained, y0 = selected.data$lat.country.obtained, x1 = selected.data$long.destination.country, y1 = selected.data$lat.destination.country, col = "red", lwd = .5)
   })
   
 })
